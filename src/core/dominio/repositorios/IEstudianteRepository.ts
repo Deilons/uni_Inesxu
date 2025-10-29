5096afa39e5b5c4289344d3d31cf832964a166c2
Deilons <apolo4748@gmail.com>
2025-10-29 00:36:59 -0500

    Feat: repositories para estudiante y profesor

---
+++ src/core/dominio/repositorios/IEstudianteRepository.ts
@@ -0,0 +1,8 @@
+import type { Estudiante } from "../usuarios/Estudiante.js";
+
+export interface IEstudianteRepository {
+    obtenerEstudiantes(): Promise<Estudiante[]>
+    obtenerEstudiantePorId(id: number): Promise<Estudiante | null>
+    guardarEstudiante(estudiante: Estudiante): Promise<Estudiante>
+    eliminarEstudiante(estudiante: Estudiante): Promise<void>
+};
---
+++ src/core/dominio/repositorios/IProfesorRepository.ts
@@ -0,0 +1,8 @@
+import type { Profesor } from "../usuarios/Profesor.js";
+
+export interface IProfesorRepository {
+    obtenerProfesores(): Promise<Profesor[]>
+    obtenerProfesorPorId(id: number): Promise<Profesor | null>
+    guardarProfesor(profesor: Profesor): Promise<Profesor>
+    eliminarProfesor(profesor: Profesor): Promise<void>
+};
